b59c03e05e9b26f7a22de72a67107de060fe2736
Rita Bindal <42319102+Rita63@users.noreply.github.com>
2024-10-16 23:00:35 +0530

    Create Arearect.py

---
+++ Arearect.py
@@ -0,0 +1,13 @@
+# Accept the length of the rectangle from the user
+length = float(input("Enter the length of the rectangle: "))
+print("Length of the rectangle:", length)
+
+# Accept the width of the rectangle from the user
+width = float(input("Enter the width of the rectangle: "))
+print("Width of the rectangle:", width)
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Print the area of the rectangle
+print("Area of the rectangle:", area)
